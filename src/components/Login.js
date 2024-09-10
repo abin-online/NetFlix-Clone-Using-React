da5bbe06bbf7ffc3cfc6d28d9bc86db52745a70b
Abin Babu <abinbabuonline@gmail.com>
2024-09-10 18:22:45 +0530

    error in enabling authentication using email/password

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,11 +2,12 @@ import React, { useRef, useState } from 'react'
 import Header from './Header'
 import { BACKGROUND_IMG } from '../util/util'
 import { checkValidData } from '../util/validate'
-
+import { auth } from '../util/firebase'
+import {createUserWithEmailAndPassword } from "firebase/auth";
 const Login = () => {
 
   const [isSigIn, setIsSignIn] = useState(true)
-  const [errMessage , setErrMessage] = useState('')
+  const [errMessage, setErrMessage] = useState('')
   const fullName = useRef(null)
   const email = useRef(null)
   const password = useRef(null)
@@ -17,9 +18,33 @@ const Login = () => {
 
   const handleButtonClick = () => {
 
-    const message = checkValidData(email.current.value , password.current.value , fullName.current.value)
+    const message = checkValidData(email.current.value, password.current.value, fullName.current.value)
     setErrMessage(message)
     console.log(errMessage)
+    if (message) return;
+
+    if (!isSigIn) {
+      //signup logic
+
+      createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
+        .then((userCredential) => {
+          // Signed up 
+          const user = userCredential.user;
+          // ...
+          console.log(user)
+
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // ..
+          setErrMessage(errorCode + "-" + errorMessage
+
+          )
+        });
+    } else {
+      //signin logic
+    }
   }
 
   return (
@@ -31,23 +56,23 @@ const Login = () => {
       >
         <div className='absolute inset-0 bg-black opacity-50 bg-gradient '></div>
       </div>
-      <form onSubmit={(e)=>e.preventDefault()} className=' w-full max-w-lg h-full absolute inset-0 p-12 bg-black my-28 mx-auto right-0 left-0 text-white opacity-80'>
+      <form onSubmit={(e) => e.preventDefault()} className=' w-full max-w-lg h-full absolute inset-0 p-12 bg-black my-28 mx-auto right-0 left-0 text-white opacity-80'>
         <h1 className='font-bold text-3xl py-4'>{isSigIn ? "Sign In" : "Sign up"}</h1>
-        { !isSigIn ?
-                  <input type='text' ref={fullName} placeholder='Full Name' className='p-2 m-2 my-1 w-full bg-gray-900 rounded-sm px-4 py-2 h-14 ' />: ""
+        {!isSigIn ?
+          <input type='text' ref={fullName} placeholder='Full Name' className='p-2 m-2 my-1 w-full bg-gray-900 rounded-sm px-4 py-2 h-14 ' /> : ""
         }
-        <p className='mx-5 text-red-600 font-normal '>{errMessage.fullNameErr} </p>
+        <p className='mx-5 text-red-600 font-normal '>{errMessage?.fullNameErr} </p>
 
         <input type='text' ref={email} placeholder='Email or phone number' className='p-2 m-2 my-6 w-full bg-gray-900 rounded-sm px-4 py-2 h-14 ' />
-        <p className='mx-5 text-red-600 font-normal '>{errMessage.emailErr} </p>
+        <p className='mx-5 text-red-600 font-normal '>{errMessage?.emailErr} </p>
 
         <input type='password' ref={password} placeholder='Password' className='p-2 m-2 my-1 w-full bg-gray-900 rounded-sm px-4 py-2 h-14 ' />
-        <p className='mx-5 text-red-600 font-normal '>{errMessage.passwordErr}</p>
+        <p className='mx-5 text-red-600 font-normal '>{errMessage?.passwordErr}</p>
         <button type='submit' className='p-2 m-2 my-6 w-full rounded-sm px-4 py-2 font-bold' style={{ backgroundColor: 'rgba(255, 0, 0, 1)' }} onClick={handleButtonClick}>
-        {isSigIn ? "Sign In" : "Sign up"}
+          {isSigIn ? "Sign In" : "Sign up"}
         </button>
         {isSigIn ?
-            <>
+          <>
             <h4 className=' flex justify-center font-semibold '>OR</h4>
             <button class="p-2 m-2 my-6 bg-white bg-opacity-30 w-full rounded-s text-white font-semibold  ">
               Use a sign-in code
@@ -55,17 +80,17 @@ const Login = () => {
         }
 
         {
-          isSigIn ? <> <h6 className=' flex justify-center font-semibold '>Forgot Password ?</h6> 
-        <span className='flex my-6'>
-          <input type='checkbox' className='form-checkbox h-5 w-5 rounded-full bg-transparent' />
-          <h6 className='px-3' >Remember me</h6>
-        </span></> : ''
+          isSigIn ? <> <h6 className=' flex justify-center font-semibold '>Forgot Password ?</h6>
+            <span className='flex my-6'>
+              <input type='checkbox' className='form-checkbox h-5 w-5 rounded-full bg-transparent' />
+              <h6 className='px-3' >Remember me</h6>
+            </span></> : ''
         }
-        {isSigIn ? <p className='my-5 ' >New to Netflix?<span className=' font-bold cursor-pointer ' onClick={toggleSignInForm}> Sign up now. </span> </p> :
+        {isSigIn ? <p className='my-5 '>New to Netflix?<span className=' font-bold cursor-pointer ' onClick={toggleSignInForm}> Sign up now. </span> </p> :
           <p className='my-5 ' >Already have an account?  <span className=' font-bold cursor-pointer ' onClick={toggleSignInForm}>Sign in now. </span> </p>
         }
       </form>
- 
+
     </div>
   )
 }
