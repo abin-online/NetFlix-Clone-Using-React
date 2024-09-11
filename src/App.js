bd802626875f81d5f104cdb3e8c9c4055e43b202
Abin Babu <abinbabuonline@gmail.com>
2024-09-11 09:21:40 +0530

    react app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import React from 'react';
 import Body from './components/Body';
 import './App.css';
 import { Provider } from 'react-redux';
+import appStore from './util/appStore'; // Ensure you are importing the appStore from the correct file
 
 function App() {
   return (
--- src/util/appStore.js
+++ src/util/appStore.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { useReducer } from './userSlice'
+import { useReducer } from "react";
 const appStore = configureStore({
     reducer: {
         user: useReducer
