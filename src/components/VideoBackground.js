eabe8f3d083d1fe641809cadc46e57f5e7c25872
Abin Babu <abinbabuonline@gmail.com>
2024-09-16 17:15:23 +0530

    video bg

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,25 +1,39 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { API_OPTIONS } from '../util/util'
 
-const VideoBackground = ({movieId}) => {
+const VideoBackground = ({ movieId }) => {
+
+    const [trailer, setTrailer] = useState('')
 
     const getMovieVideos = async () => {
-        const data = await fetch('https://api.themoviedb.org/3/movie/365177/videos?language=en-US' ,API_OPTIONS)
+        const data = await fetch('https://api.themoviedb.org/3/movie/365177/videos?language=en-US', API_OPTIONS)
         const json = await data.json();
-        console.log(json)
-        const filteredData = json.results.filter((video)=> video.type === 'Teaser')
-        const trailer =  filteredData.length === 0 ? filteredData[0] : json.results[0]
-        console.log(trailer)
+        console.log('json data',json.results)
+        const filteredData = json.results.filter((video) => video.type === 'Teaser')
+        console.log("filteredData    ", filteredData  )
+        const selectedTrailer = filteredData.length === 0 ? filteredData[0] : json.results[0]
+        console.log("trailerr ",selectedTrailer)
+
+        if (selectedTrailer) {
+            setTrailer(selectedTrailer.key);
+            console.log("Selected trailer key:", selectedTrailer.key);
+        }
     };
 
-    useEffect(()=> {
+    useEffect(() => {
         getMovieVideos()
-    },[])
-  return (
-    <div>
-      
-    </div>
-  )
+    }, [])
+    console.log(trailer)
+    return (
+        <div className='w-screen'>
+            <iframe className='w-screen aspect-video'
+                src={`https://www.youtube.com/embed/${trailer}?autoplay=1&mute=1&loop=1&playlist=${trailer}`}
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerpolicy="strict-origin-when-cross-origin" >
+            </iframe>
+        </div>
+    )
 }
 
 export default VideoBackground
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const VideoTitle = ({title , overview}) => {
   return (
-    <div className='pt-36 px-12'>
+    <div className='w-screen aspect-video pt-[18%]  px-24 absolute text-white bg-gradient-to-r from-black '>
         <h1 className='text-6xl font-bold'>{title}</h1>
         <p className='py-6 text-lg w-1/4'>{overview}</p>
         <div>
-            <button className='bg-gray-500 text-black p-4 px-12 text-lg bg-opacity-50 rounded-lg'>Play ▶</button>
-            <button className=' mx-2 bg-gray-500 text-black p-4 px-12 text-lg bg-opacity-20 rounded-lg' >More Info</button>
+            <button className='bg-white text-black p-4 px-12 text-lg bg rounded-lg hover:bg-opacity-80'>▶ Play</button>
+            <button className='text-white mx-2 bg-gray-500  p-4 px-12 text-lg bg-opacity-20 rounded-lg' >ℹ More Info</button>
         </div>
     </div>
   )
